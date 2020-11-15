8628c5c9f7cf55b83e291acf69632e8afa00118f
Huikang Huang <huikangh@uci.edu>
2020-11-14 20:05:11 -0800

    all files

--- PycharmProjects/Checkers_Project/src/checkers-python/MCTS.py
+++ PycharmProjects/Checkers_Project/src/checkers-python/MCTS.py
@@ -35,45 +35,40 @@ class MonteCarlo:
             # randomly pick a move for ourself
             my_moves = []
             all_moves = board_copy.get_all_possible_moves(self.player)
-            # print(all_moves)
+
             for i in all_moves:
                 for j in i:
                     my_moves.append(j)
             if my_moves:
                 move = choice(my_moves)
                 board_copy.make_move(move, self.player)
-            board_copy.show_board()
+            #board_copy.show_board()
 
             if expand and board_copy not in self.plays:
                 expand = False
                 self.plays[board_copy] = 0
                 self.wins[board_copy] = 0
-
             visited_boards.add(board_copy)
 
-            win = board_copy.is_win(self.player)
-            if win != 0:
-                winner = win
+            winner = board_copy.is_win(self.player)
+            if winner != 0:
                 break
 
             # randomly pick a move for the opponent
             opp_moves = []
             all_moves = board_copy.get_all_possible_moves(self.opponent[self.player])
-            # print(all_moves)
+
             for i in all_moves:
                 for j in i:
                     opp_moves.append(j)
             if opp_moves:
                 move = choice(opp_moves)
                 board_copy.make_move(move, self.opponent[self.player])
-            board_copy.show_board()
+            #board_copy.show_board()
 
-            win = board_copy.is_win(self.player)
-            if win != 0:
-                winner = win
+            winner = board_copy.is_win(self.opponent[self.player])
+            if winner != 0:
                 break
-
-        print(winner)
 
         for board in visited_boards:
             if board not in self.plays:
